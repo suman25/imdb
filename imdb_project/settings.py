72b68459285f5c05085c010e13fe10496fe51fdd
Suman Daryani <sumandaryani125@gmail.com>
2020-10-29 20:15:07 +0530

    added django_heroku

--- imdb_project/settings.py
+++ imdb_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'g6^*+3-p=$i1e6(^)bvyo@tx_9use#_gf_g@&^j4$*^4&r&4)r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+import django_heroku
 ALLOWED_HOSTS = []
 
 
@@ -131,3 +131,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+django_heroku.settings(locals())
