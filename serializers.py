a6ea49066b9933313bf4cff9a50fd796184804be
suman25 <sumandaryani125@gmail.com>
2020-10-29 18:09:18 +0530

    Delete serializers.py

--- serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from moviesapp.models import Movies
-
-
-class MovieSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Movies
-        fields = ('name','popularity','director','imdb_score')
